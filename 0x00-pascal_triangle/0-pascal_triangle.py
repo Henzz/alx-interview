5ecc6166fc2eb7976a2ec3a97fda7eec913f59bb
Henzz <khenok53@gmail.com>
2024-06-28 01:03:51 +0300

    task 0 method update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """ Returns a list of lists of integers. """
-
-
 def factorial(n):
     """ Factorial function. """
     # Base condition to end recursion
@@ -40,17 +38,3 @@ def pascal_triangle(n):
     # Return list of list
     return matrix
 
-#!/usr/bin/python3
-#def pascal_triangle(n):
-#    if n <= 0:
-#        return []
-
-#    triangle = [[1]]
-#    for i in range(1, n):
-#        row = [1]
-#        for j in range(1, i):
-#            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-#        row.append(1)
-#        triangle.append(row)
-#
-#    return triangle
