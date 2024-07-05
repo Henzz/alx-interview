6debdc08e6c69608db7d0f993a363fa244161c1f
Henzz <khenok53@gmail.com>
2024-07-06 02:24:31 +0300

    task 0 done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,42 @@
-#!/usr/bin/python3
-def canUnlockAll(boxes):
+#!/usr/bin/env python3
+"""
+This module provides a function to determine if all boxes can be unlocked.
+"""
+
+from typing import List
+
+
+def canUnlockAll(boxes: List[List[int]]) -> bool:
+    """
+    Determine if all boxes can be unlocked.
+
+    Args:
+        boxes (List[List[int]]): A list of lists representing the boxes
+        and their keys.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
     n = len(boxes)
-    unlocked = [False] * n
-    unlocked[0] = True  # The first box is always unlocked
+    opened = [False] * n
+    opened[0] = True
+    keys = boxes[0].copy()
 
-    queue = [0]  # Start with the first box
+    while keys:
+        key = keys.pop()
+        if 0 <= key < n and not opened[key]:
+            opened[key] = True
+            keys.extend(boxes[key])
 
-    while queue:
-        current = queue.pop(0)
+    return all(opened)
 
-        for key in boxes[current]:
-            if key < n and not unlocked[key]:
-                unlocked[key] = True
-                queue.append(key)
 
-    return all(unlocked)
+if __name__ == "__main__":
+    boxes1 = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes1))  # Expected output: True
+
+    boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes2))  # Expected output: True
+
+    boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes3))  # Expected output: False
